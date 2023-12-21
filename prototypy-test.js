4f588cfd8e88837e495a27c9a1697162901a7438
Will Wang <112908507+littleq101010@users.noreply.github.com>
2023-12-20 19:29:40 -0800

    Assignment of Module2

---
+++ prototypy-test.js
@@ -0,0 +1,23 @@
+// function Circle (radius) {
+//     this.radius = radius;
+// }
+
+// Circle.prototype.getArea = 
+// function () {
+//     return Math.PI * Math.pow(this.radius, 2);
+//  };
+
+// var myCircle = new Circle(10);
+// console.log(myCircle);
+
+
+function Circle(radius) {
+    this.radius = radius;
+}
+
+Circle.prototype.getArea = function () {
+    return Math.PI * Math.pow(this.radius, 2);
+};
+
+var myCircle = new Circle(10);
+console.log(myCircle);
